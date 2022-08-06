d292cbf7673e3f0465141e88f3d36b92831edc4f
Lewis Farnworth <lewfar99@gmail.com>
2022-08-06 03:47:57 +0100

    Added message_codes property to the message index, which exports the canonical message codes for each message type, other than the header.

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -5,3 +5,9 @@ export const inbound_messages = {
     header,
     door,
 };
+
+/** This is useful because the header does not have a message code */
+type MessagesWithoutHeader = Omit<typeof inbound_messages, "header">;
+
+export const message_codes = new Map<number, keyof MessagesWithoutHeader>();
+message_codes.set(0x0D, "door");
